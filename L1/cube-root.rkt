aff766c0f9f7524a72eac956fc444c4c210b66c3
T3sT3ro <max@polarczyk.pl>
2018-02-22 14:21:43 +0100

    Tweak L1/cube-root

--- L1/cube-root.rkt
+++ L1/cube-root.rkt
@@ -1,18 +1,45 @@
 #lang racket
 
+;; author Tooster aka Maksymilian Polarczyk
+
+;; returns x to the power of three
 (define (cube x) (* x x x))
 
-
+;; calculates approximation of cube root from x
 (define (cube-root x)
+  ;; precision of rounding
+  (define precission 0.001)
   ;; iterates over approximation
   (define (approximate approx)
-    (if (< (abs (- x (cube approx))) (cube 0.001))
+    (if (< (abs (- x (cube approx))) (cube precission))
         approx
         [approximate (improve approx)]))
 
   ;; improves approximation
   (define (improve approx)
     (/ (+ (/ x (* approx approx)) (* 2 approx)) 3))
-  
-  (approximate 1.0)
+
+  ;; for negative do the same reversing the sign
+  (if (< x 0)
+      [* -1 (cube-root (* -1 x))]
+      [approximate 1.0]
+  )
 )
+
+;; tests
+(cube-root 0)
+(cube-root 8)
+(cube-root 9)
+(cube-root 27)
+(cube-root 81)
+(cube-root 125)
+(cube-root 1000)
+(cube-root 0.125)
+(cube-root 0.027)
+(cube-root -8)
+(cube-root -3)
+(cube-root -27)
+(cube-root -1)
+(cube-root 1)
+(cube-root -125)
+(cube-root -1000) 
