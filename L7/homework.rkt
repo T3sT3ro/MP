2133ae9a34914af3daef07efa848e1bb5ff532c9
Tooster <max@polarczyk.pl>
2018-04-15 16:26:19 +0200

    Optimize let-lift from O(n^2) to O(n), cosmetic fix

--- L7/homework.rkt
+++ L7/homework.rkt
@@ -1,5 +1,4 @@
 #lang racket
-(require racket/trace)
 
 ;; expressions
 
@@ -96,37 +95,12 @@
 
 (define (env-for-let def env)
   (add-to-env
-    (let-def-var def)
-    (eval-env (let-def-expr def) env)
-    env))
+   (let-def-var def)
+   (eval-env (let-def-expr def) env)
+   env))
 
 (define (eval e)
   (eval-env e empty-env))
-
-;; procedure substituting unbound symbol in expression
-
-(define (subst-free e symbol new-symbol)
-  (define (f e symbol new-symbol bound-vars)
-    (cond [(const? e) e]
-          [(var? e) (if [and (eq? (var-var e) symbol)
-                             (not (member symbol bound-vars))]
-                        new-symbol
-                        (var-var e))]
-          [(op? e) (op-cons (op-op e) (map (lambda (x) (f x symbol new-symbol bound-vars))
-                          (op-args e)))]
-          [(let? e) (let-cons (let-def-cons (let-def-var (let-def e))
-                                            (f (let-def-expr (let-def e)) symbol new-symbol bound-vars))
-                              (f (let-expr e) symbol new-symbol (cons (let-def-var (let-def e)) bound-vars)))]))
-  (f e symbol new-symbol null))
-
-  ;; preprocess input expression to change variable names so there won't be collisions with algorithm
-  (define (preprocess t)
-    (cond [(const? t) t]
-          [(var? t) t]
-          [(op? t) (op-cons (op-op t) (map preprocess (op-args t)))]
-          [(let? t) (let* ([old (let-def-var (let-def t))]
-                           [new (string->symbol (string-append "_" (symbol->string old)))])
-                      (let-cons (let-def-cons new (preprocess (let-def-expr (let-def t)))) (preprocess (subst-free (let-expr t) old new))))]))
 
 ;; let-lift procedure
 (define (let-lift e)
@@ -141,36 +115,37 @@
     (if [null? defs] expr (let-cons (car defs) (wrap expr (cdr defs)))))
 
   ;; transforms list of op-args into ret as (list of rets from subexprs., definitions, newest counter)
-  (define (arith/let-list->ret-list xs cntr)
+  (define (arith/let-list->ret-list xs cntr env)
     (if [null? xs]
         (ret-cons null null cntr)
-        (let* ([ret (arith/let->ret (car xs) cntr)]
-               [xs-ret (arith/let-list->ret-list (cdr xs) (ret-cntr ret))])
+        (let* ([ret (arith/let->ret (car xs) cntr env)]
+               [xs-ret (arith/let-list->ret-list (cdr xs) (ret-cntr ret) env)])
           (ret-cons (cons (ret-expr ret) (ret-expr xs-ret)) ;; expr will be list of ret's 
                     (append (ret-defs ret) (ret-defs xs-ret)) ;; concat definitions in disjoint subexpressions
                     (ret-cntr xs-ret)))))
   
   ;; tranforms to intermediate result structure: res
-  (define (arith/let->ret e cntr) ;; produces ret struct which has arith expressions, list of definitions and cntr for new symbol
+  (define (arith/let->ret e cntr env) ;; produces ret struct which has arith expressions, list of definitions and cntr for new symbol
     (cond [(const? e) (ret-cons e null cntr)]
-          [(var? e) (ret-cons e null cntr)]
-          [(op? e) (let ([ret (arith/let-list->ret-list (op-args e) cntr)]) ;; process op-args
+          [(var? e) (ret-cons (find-in-env e env) null cntr)]
+          [(op? e) (let ([ret (arith/let-list->ret-list (op-args e) cntr env)]) ;; process op-args
                      (ret-cons (op-cons (op-op e) (ret-expr ret))
                                (ret-defs ret)
                                (ret-cntr ret)))]
-          [(let? e) (let* ([def-ret (arith/let->ret (let-def-expr (let-def e)) cntr)] ;; proper return from definitions expr
+          [(let? e) (let* ([def-ret (arith/let->ret (let-def-expr (let-def e)) cntr env)] ;; proper return from definitions expr
                            [current-cntr (ret-cntr def-ret)] ;; new available counter for variables
-                           [expr-ret (arith/let->ret (subst-free (let-expr e) ;; substitute bound variable to new uniqe identifier
-                                                                 (var-var (let-def-var (let-def e)))
-                                                                 (number->symbol current-cntr))
-                                                     (add1 current-cntr))]);; return from expr
+                           [expr-ret (arith/let->ret (let-expr e)
+                                                     (add1 current-cntr)
+                                                     (add-to-env (var-var (let-def-var (let-def e)))
+                                                                 (number->symbol current-cntr)
+                                                                 env))]);; return from expr
                       (ret-cons (ret-expr expr-ret) ;; let expresison as ret-expr
                                 (append (ret-defs def-ret) ;; append definitions from def expr, def var and expr
                                         (list (let-def-cons (number->symbol current-cntr) (ret-expr def-ret))) ;; let's definition
                                         (ret-defs expr-ret))
                                 (ret-cntr expr-ret)))])) ;; update counter after this let
   (if [arith/let-expr? e]
-      (let ([ret (arith/let->ret (preprocess e) 0)])
+      (let ([ret (arith/let->ret e 0 empty-env)])
         (wrap (ret-expr ret) (ret-defs ret))) ;; wrap arith-expr into lets according to defs
       (error "Malformed arith/let-expr expression")))
 
@@ -181,7 +156,7 @@
     (let* ([r1 (random 100)]
            [r1 (if [= depth 1] (modulo r1 20) r1)])
       (cond [(< r1 5) (random 100)] ;; 5% chance 
-            [(< r1 20) (if [null? bound] ;; 15 % chance
+            [(< r1 30) (if [null? bound] ;; 15 % chance
                            (random 100)
                            (var-cons (car (shuffle bound))))]
             [(< r1 50) (cons (car (shuffle '(+ - *))) ;; without '/ to avoid zero division
@@ -199,12 +174,15 @@
         (and (let-lifted-expr? lifted)
              (= (eval e) (eval lifted))))
       #t
-      (begin (display "failed: ")(display e) (display "\n"))))
+      (printf "failed: ~a\n" e)))
 
 (display "Fixed test pass status: ")
 (and (test-expr '(let (x (let (x 2) (+ x (let (y x) (let (x 3) (* x y))))))
                    (let (x 3) (let (y 5) (+ (let (x 2) (+ x y)) (let (x 1) (+ x y)))))))
-     (test-expr '(let (x1 1) (let (x2 2) (+ x1 x2)) )))
+     (test-expr '(let (x1 1) (let (x2 2) (+ x1 x2)) ))
+     (test-expr '(let (x1 1) (+ x1 (let (x2 x1) (let (x1 7) (+ x1 x2))))))
+     (test-expr '(let (x 3) (+ x (let (x 7) x)))))
 
 (define (test)  
-    (andmap test-expr (build-list 1000 (lambda (x) (gen-expression (random 1 10))))))
+  (printf "Test 1000 random expressions: ~a\n"
+          (andmap test-expr (build-list 1000 (lambda (x) (gen-expression (random 1 10)))))))
