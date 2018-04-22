74b7b52759c106133390ce192717bf10c2201069
Tooster <max@polarczyk.pl>
2018-04-22 18:04:13 +0200

    Add L8 homework task A WITHOUT tests

---
+++ L8/homework.rkt
@@ -0,0 +1,504 @@
+#lang racket
+
+;; pomocnicza funkcja dla list tagowanych o określonej długości
+
+(define (tagged-tuple? tag len p)
+  (and (list? p)
+       (= (length p) len)
+       (eq? (car p) tag)))
+
+(define (tagged-list? tag p)
+  (and (pair? p)
+       (eq? (car p) tag)
+       (list? (cdr p))))
+
+;; self-evaluating expressions
+
+(define (const? t)
+  (or (number? t)
+      (my-symbol? t)
+      (eq? t 'true)
+      (eq? t 'false)))
+
+;; ADDED: "number?" predicate
+
+(define (my-number? t)  (number? t))
+(define (number?? t)  (tagged-tuple? 'number? 2 t))
+(define (number??-expr t) (second t))
+
+;; ADDED: boolean "and", "or"
+(define (and? t)  (tagged-list? 'and t))
+(define (and-args t)  (cdr t))
+
+(define (or? t)  (tagged-list? 'or t))
+(define (or-args t)  (cdr t))
+
+;; arithmetic expressions
+
+(define (op? t)
+  (and (list? t)
+       (member (car t) '(+ - * / = > >= < <= eq? not)))) ;; ADDED: "not"
+
+(define (op-op e)
+  (car e))
+
+(define (op-args e)
+  (cdr e))
+
+(define (op-cons op args)
+  (cons op args))
+
+(define (op->proc op)
+  (cond [(eq? op '+) +]
+        [(eq? op '*) *]
+        [(eq? op '-) -]
+        [(eq? op '/) /]
+        [(eq? op '=)  (compose bool->val =)]
+        [(eq? op '>)  (compose bool->val >)]
+        [(eq? op '>=) (compose bool->val >=)]
+        [(eq? op '<)  (compose bool->val <)]
+        [(eq? op '<=) (compose bool->val <=)]
+        [(eq? op 'eq?) (lambda (x y)
+                         (bool->val (eq? (symbol-symbol x)
+                                         (symbol-symbol y))))]
+        [(eq? op 'not) (lambda (x) (bool->val (not (val->bool x))))])) ;; ADDED: "not"
+
+;; symbols
+
+(define (my-symbol? e)
+  (and (tagged-tuple? 'quote 2 e)
+       (symbol? (second e))))
+
+(define (symbol-symbol e)
+  (second e))
+
+(define (symbol-cons s)
+  (list 'quote s))
+
+;; lets
+
+(define (let-def? t)
+  (and (list? t)
+       (= (length t) 2)
+       (symbol? (car t))))
+
+(define (let-def-var e)
+  (car e))
+
+(define (let-def-expr e)
+  (cadr e))
+
+(define (let-def-cons x e)
+  (list x e))
+
+(define (let? t)
+  (and (tagged-tuple? 'let 3 t)
+       (let-def? (cadr t))))
+
+(define (let-def e)
+  (cadr e))
+
+(define (let-expr e)
+  (caddr e))
+
+(define (let-cons def e)
+  (list 'let def e))
+
+;; variables
+
+(define (var? t)
+  (symbol? t))
+
+(define (var-var e)
+  e)
+
+(define (var-cons x)
+  x)
+
+;; pairs
+
+(define (cons? t)
+  (tagged-tuple? 'cons 3 t))
+
+(define (cons-fst e)
+  (second e))
+
+(define (cons-snd e)
+  (third e))
+
+(define (cons-cons e1 e2)
+  (list 'cons e1 e2))
+
+(define (car? t)
+  (tagged-tuple? 'car 2 t))
+
+(define (car-expr e)
+  (second e))
+
+(define (cdr? t)
+  (tagged-tuple? 'cdr 2 t))
+
+(define (cdr-expr e)
+  (second e))
+
+(define (pair?? t)
+  (tagged-tuple? 'pair? 2 t))
+
+(define (pair?-expr e)
+  (second e))
+
+(define (pair?-cons e)
+  (list 'pair? e))
+
+
+;; if
+
+(define (if? t)
+  (tagged-tuple? 'if 4 t))
+
+(define (if-cons b t f)
+  (list 'if b t f))
+
+(define (if-cond e)
+  (second e))
+
+(define (if-then e)
+  (third e))
+
+(define (if-else e)
+  (fourth e))
+
+;; cond
+
+(define (cond-clause? t)
+  (and (list? t)
+       (= (length t) 2)))
+
+(define (cond-clause-cond c)
+  (first c))
+
+(define (cond-clause-expr c)
+  (second c))
+
+(define (cond-clause-cons b e)
+  (list b e))
+
+(define (cond? t)
+  (and (tagged-list? 'cond t)
+       (andmap cond-clause? (cdr t))))
+
+(define (cond-clauses e)
+  (cdr e))
+
+(define (cond-cons cs)
+  (cons 'cond cs))
+
+;; lists
+
+(define (my-null? t)
+  (eq? t 'null))
+
+(define (null?? t)
+  (tagged-tuple? 'null? 2 t))
+
+(define (null?-expr e)
+  (second e))
+
+(define (null?-cons e)
+  (list 'null? e))
+
+;; lambdas
+
+(define (lambda? t)
+  (and (tagged-tuple? 'lambda 3 t)
+       (list? (cadr t))
+       (andmap symbol? (cadr t))))
+
+(define (lambda-cons vars e)
+  (list 'lambda vars e))
+
+(define (lambda-vars e)
+  (cadr e))
+
+(define (lambda-expr e)
+  (caddr e))
+
+;; lambda-rec
+
+(define (lambda-rec? t)
+  (and (tagged-tuple? 'lambda-rec 3 t)
+       (list? (cadr t))
+       (>= (length (cadr t)) 1)
+       (andmap symbol? (cadr t))))
+
+(define (lambda-rec-cons vars e)
+  (list 'lambda-rec vars e))
+
+(define (lambda-rec-expr e)
+  (third e))
+
+(define (lambda-rec-name e)
+  (car (second e)))
+
+(define (lambda-rec-vars e)
+  (cdr (second e)))
+
+;; applications
+
+(define (app? t)
+  (and (list? t)
+       (> (length t) 0)))
+
+(define (app-cons proc args)
+  (cons proc args))
+
+(define (app-proc e)
+  (car e))
+
+(define (app-args e)
+  (cdr e))
+
+;; expressions
+
+(define (expr? t)
+  (or (const? t)
+      (and (and? t)
+           (andmap expr? (and-args t)))
+      (and (or? t)
+           (andmap expr? (or-args t)))
+      (and (op? t)
+           (andmap expr? (op-args t)))
+      (and (let? t)
+           (expr? (let-expr t))
+           (expr? (let-def-expr (let-def t))))
+      (and (cons? t)
+           (expr? (cons-fst t))
+           (expr? (cons-snd t)))
+      (and (car? t)
+           (expr? (car-expr t)))
+      (and (cdr? t)
+           (expr? (cdr-expr t)))
+      (and (pair?? t)
+           (expr? (pair?-expr t)))
+      (my-null? t)
+      (and (null?? t)
+           (expr? (null?-expr t)))
+      (and (if? t)
+           (expr? (if-cond t))
+           (expr? (if-then t))
+           (expr? (if-else t)))
+      (and (cond? t)
+           (andmap (lambda (c)
+                      (and (expr? (cond-clause-cond c))
+                           (expr? (cond-clause-expr c))))
+                   (cond-clauses t)))
+      (and (lambda? t)
+           (expr? (lambda-expr t)))
+      (and (lambda-rec? t)
+           (expr? (lambda-rec-expr t)))
+      (var? t)
+      (and (app? t)
+           (expr? (app-proc t))
+           (andmap expr? (app-args t)))))
+
+;; environments
+
+(define empty-env
+  null)
+
+(define (add-to-env x v env)
+  (cons (list x v) env))
+
+(define (find-in-env x env)
+  (cond [(null? env) (error "undefined variable" x)]
+        [(eq? x (caar env)) (cadar env)]
+        [else (find-in-env x (cdr env))]))
+
+;; closures
+
+(define (closure-cons xs expr env)
+  (list 'closure xs expr env))
+
+(define (closure? c)
+  (and (list? c)
+       (= (length c) 4)
+       (eq? (car c) 'closure)))
+
+(define (closure-vars c)
+  (cadr c))
+
+(define (closure-expr c)
+  (caddr c))
+
+(define (closure-env c)
+  (cadddr c))
+
+;; closure-rec
+
+(define (closure-rec? t)
+  (tagged-tuple? 'closure-rec 5 t))
+
+(define (closure-rec-name e)
+  (second e))
+
+(define (closure-rec-vars e)
+  (third e))
+
+(define (closure-rec-expr e)
+  (fourth e))
+
+(define (closure-rec-env e)
+  (fifth e))
+
+(define (closure-rec-cons f xs e env)
+  (list 'closure-rec f xs e env))
+
+;; evaluator
+
+(define (bool->val b)
+  (if b 'true 'false))
+
+(define (val->bool s)
+  (cond [(eq? s 'true)  true]
+        [(eq? s 'false) false]
+        [else (error "could not convert symbol to bool")]))
+
+(define (eval-env e env)
+  (cond [(const? e)
+         e]
+        [(number?? e) ;; ADDED: "number?"
+         (bool->val (my-number? (eval-env (number??-expr e) env)))]
+        [(and? e) ;; ADDED: "and"
+         (bool->val (andmap (lambda (x) (val->bool (eval-env x env))) (and-args e)))]
+        [(or? e) ;; ADDED: "or"
+         (bool->val (ormap (lambda (x) (val->bool (eval-env x env))) (and-args e)))]
+        [(op? e)
+         (apply (op->proc (op-op e))
+                (map (lambda (a) (eval-env a env))
+                     (op-args e)))]
+        [(let? e)
+         (eval-env (let-expr e)
+                   (env-for-let (let-def e) env))]
+        [(my-null? e)
+         null]
+        [(cons? e)
+         (cons (eval-env (cons-fst e) env)
+               (eval-env (cons-snd e) env))]
+        [(car? e)
+         (car (eval-env (car-expr e) env))]
+        [(cdr? e)
+         (cdr (eval-env (cdr-expr e) env))]
+        [(pair?? e)
+         (bool->val (pair? (eval-env (pair?-expr e) env)))]
+        [(null?? e)
+         (bool->val (null? (eval-env (null?-expr e) env)))]
+        [(if? e)
+         (if (val->bool (eval-env (if-cond e) env))
+             (eval-env (if-then e) env)
+             (eval-env (if-else e) env))]
+        [(cond? e)
+         (eval-cond-clauses (cond-clauses e) env)]
+        [(var? e)
+         (find-in-env (var-var e) env)]
+        [(lambda? e)
+         (closure-cons (lambda-vars e) (lambda-expr e) env)]
+        [(lambda-rec? e)
+         (closure-rec-cons (lambda-rec-name e)
+                           (lambda-rec-vars e)
+                           (lambda-rec-expr e)
+                           env)]
+        [(app? e)
+         (apply-closure
+           (eval-env (app-proc e) env)
+           (map (lambda (a) (eval-env a env))
+                (app-args e)))]))
+
+(define (eval-cond-clauses cs env)
+  (if (null? cs)
+      (error "no true clause in cond")
+      (let ([cond (cond-clause-cond (car cs))]
+            [expr (cond-clause-expr (car cs))])
+           (if (val->bool (eval-env cond env))
+               (eval-env expr env)
+               (eval-cond-clauses (cdr cs) env)))))
+
+(define (apply-closure c args)
+  (cond [(closure? c)
+         (eval-env
+            (closure-expr c)
+            (env-for-closure
+              (closure-vars c)
+              args
+              (closure-env c)))]
+        [(closure-rec? c)
+         (eval-env
+           (closure-rec-expr c)
+           (add-to-env
+            (closure-rec-name c)
+            c
+            (env-for-closure
+              (closure-rec-vars c)
+              args
+              (closure-rec-env c))))]))
+
+(define (env-for-closure xs vs env)
+  (cond [(and (null? xs) (null? vs)) env]
+        [(and (not (null? xs)) (not (null? vs)))
+         (add-to-env
+           (car xs)
+           (car vs)
+           (env-for-closure (cdr xs) (cdr vs) env))]
+        [else (error "arity mismatch")]))
+
+(define (env-for-let def env)
+  (add-to-env
+    (let-def-var def)
+    (eval-env (let-def-expr def) env)
+    env))
+
+(define (eval e)
+  (eval-env e empty-env))
+
+#|
+Zadanie A to napisanie ewaluatora wyrażeń arytmetycznych w interpretowanym
+języku z wykładu. Składnia abstrakcyjna tych wyrażeń powinna być dana 
+analogicznie do składni abstrakcyjnej wyrażeń arytmetycznych, które ewalu-
+owaliśmy w Rackecie: stałe jako liczby, a operatory binarne jako trzyelementowe
+listy zawierające symbol reprezentujący operację i dwa podwyrazenia.
+Uwaga: Napisanie ewalatora nie powinno by trudne, w końcu widzieliśmy
+podobny kawałek kodu (ale w innym języku) na wykładzie. Trudnością w tym
+zadaniu jest zorientowanie się, co jest bytem w którym języku.
+Kolejna uwaga: Nasz język jest nadal dość ubogi. Jeśli będzie wygodniej
+najpierw rozbudowa go o jakąś formę, która sprawi, że będzie łatwiej napisać 
+ewaluator (albo łatwiej konstruować wyrazenia arytmetyczne w celu testowania), 
+proszę tak zrobić.
+
+|#
+
+
+;; Added to language: "and", "or", "not", "number?" 
+;; Returns arithmetic expression evaluator in our language
+(define lang-arith-evaluator
+  '{let (lang-arith-number? (lambda (x) (number? x))) ;; OK
+     (let (lang-arith-op-proc (lambda (x) (cond [(eq? x (quote +))
+                                                 (lambda (a b) (+ a b))]
+                                                [(eq? x (quote -))
+                                                 (lambda (a b) (- a b))]
+                                                [(eq? x (quote *))
+                                                 (lambda (a b) (* a b))]
+                                                [(eq? x (quote /))
+                                                 (lambda (a b) (+ a b))])))
+       (lambda-rec (arith-eval e)
+                   (if (lang-arith-number? e)
+                       e
+                       ((lang-arith-op-proc (car e)) (arith-eval (car (cdr e)))
+                                                     (arith-eval (cdr (cdr e)))))))})
+
+
+
+
+
+
+
+
+
